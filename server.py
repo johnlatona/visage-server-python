5acb0df791d13524c0a0d853bb60da8ab5419849
John Latona <John.Latona@bloomingdales.com>
2019-06-17 17:08:11 -0400

    updated server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import http.server
 import socketserver
+import types
 from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 from visage import ApplyMakeup
@@ -11,22 +12,22 @@ CORS(app)
 @app.route("/api", methods=['POST'])
 def hello():
     content = request.get_json()
-    r = content.get('R')
-    g = content.get('G')
-    b = content.get('B')
     imgPath = content.get('imgPath')
-
-    print(r)
-    print(g)
-    print(b)
-    AM = ApplyMakeup()
-    output_file = AM.apply_lipstick(imgPath,float(r),float(g),float(b))
-    # print('ooutput file', output_file)
-    # file_output = f"output_color_{r}_{g}_{b}.jpg"
-    # filename = os.path.join(app.instance_path, '', file_output)
-    return jsonify({
-      "fileLocation": f"{app.root_path}/output_color_{r}_{g}_{b}.jpg"
-    })
+    if isinstance(content.get('colors'), list):
+        colorHexes = content.get('colors')
+        colorList = []
+        for color in colorHexes:
+            r = color.get('R')
+            g = color.get('G')
+            b = color.get('B')
+            AM = ApplyMakeup()
+            output_file = AM.apply_lipstick(imgPath,float(r),float(g),float(b))
+            fileLocation = {
+                "fileLocation": f"{app.root_path}/output_color_{r}_{g}_{b}.jpg"
+            }
+            colorList.append(fileLocation)
+        print(colorList)
+        return jsonify(colorList)
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
